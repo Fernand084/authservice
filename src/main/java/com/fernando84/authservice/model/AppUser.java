39962b2728d620ac3008c75b9daba9f8a18c9095
Fernand084 <edgar.lopezr84@outlook.com>
2026-08-10 19:50:01 -0600

    Models Role and AppUser. This classes remain the same as the monolit versions, only removing the schema = "app_security" as the DB is exclusive of this service

---
+++ src/main/java/com/fernando84/authservice/model/AppUser.java
@@ -0,0 +1,27 @@
+package com.fernando84.authservice.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "app_user")
+@Getter
+@Setter
+@NoArgsConstructor
+public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
---
+++ src/main/java/com/fernando84/authservice/model/Role.java
@@ -0,0 +1,6 @@
+package com.fernando84.authservice.model;
+
+public enum Role {
+    ADMIN,
+    USER
+}
